759f55551be17d1fcdc1d2da8233a58ed7333710
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-03 11:20:50 +0500

    File size configure in Program.cs; Adding web.config file to Project (for hosting config at the future deployments);

--- UploadAndDownloadFile/Program.cs
+++ UploadAndDownloadFile/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.FileProviders;
 using System.Security.Cryptography.Xml;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// file size configure
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.ValueLengthLimit = int.MaxValue;
+    options.MultipartBodyLengthLimit = int.MaxValue;
+    options.MemoryBufferThreshold = int.MaxValue;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
